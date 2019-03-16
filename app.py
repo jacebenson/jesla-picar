54f569d26bbf539681dd08b57d675614cc65bd67
Jace Benson <jace.benson@protonmail.com>
2019-03-16 05:40:00 +0000

    added bakcward

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+import explorerhat
 from flask import Flask
 from flask import jsonify
 app = Flask(__name__)
-
+class DataStore():
+    speed = 0
+    mod = 5
+    direction = 'forwards'
+data = DataStore()
 
 @app.route('/')
 def hello():
@@ -9,9 +14,28 @@ def hello():
 
 @app.route('/car/forward')
 def forward():
-    print('forward')
-    data = jsonify({'car': 'forwards'})
-    return data
+    if data.speed+data.mod<=100:
+        data.speed = data.speed + data.mod
+    if data.speed>=0:
+        data.direction = 'forwards'
+        explorerhat.motor.forwards(data.speed)
+    else:
+        data.direction = 'backwards'
+        explorerhat.motor.backwards(data.speed*-1)
+    jsonData = jsonify({'direction': data.direction, 'speed': data.speed})
+    return jsonData
+@app.route('/car/backward') 
+def backward():
+     if data.speed+data.mod<=-100:
+         data.speed = data.speed - data.mod
+     if data.speed>=0:
+         data.direction = 'forwards'
+         explorerhat.motor.forwards(data.speed)
+     else:
+         data.direction = 'backwards'
+         explorerhat.motor.backwards(data.speed*-1)
+     jsonData = jsonify({'direction': data.direction, 'speed': data.speed})
+     return jsonData
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=80, debug=True)
